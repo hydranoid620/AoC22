b3509790f02078fad6681704244407f4532f18e6
Nicholas Rosati <nicholas@rosati.xyz>
2022-11-30 15:09:12 -0500

    common lib finished

--- CommonLib/CommonLib.cs
+++ CommonLib/CommonLib.cs
@@ -1,4 +1,5 @@
-﻿namespace CommonLib;
+﻿// ReSharper disable MemberCanBePrivate.Global
+namespace CommonLib;
 
 public static class CommonLib
 {
@@ -7,7 +8,7 @@ public static class CommonLib
     /// </summary>
     /// <param name="filePath">Full path to input file</param>
     /// <returns>List of strings where each line in the file is an entry in the list</returns>
-    public static List<string> ReadFile(string filePath)
+    public static IEnumerable<string> ReadFile(string filePath)
     {
         return new List<string>(File.ReadAllLines(filePath));
     }
@@ -17,20 +18,31 @@ public static class CommonLib
     /// </summary>
     /// <param name="filePath">Full path to input file</param>
     /// <returns>A list of integers where each line in the file is a number in the list</returns>
-    public static List<int> ReadFileAsInts(string filePath)
+    public static IEnumerable<int> ReadFileAsInts(string filePath)
     {
-        
+        return ReadFile(filePath).Select(int.Parse).ToList();
     }
 
     /// <summary>
-    /// Reads a list of numbers from a file and splits each line at a separator, returning a 2D array
+    /// Reads a list of strings from a file and splits each line at a separator, returning a 2D array
+    /// </summary>
+    /// <param name="filePath">Full path to input file</param>
+    /// <param name="separator">Separator to split each line at. Uses a space by default</param>
+    /// <returns>A 2D list of strings where the first dimension is a line in the file and the second dimension is each word on the line</returns>
+    public static IEnumerable<IEnumerable<string>> ReadListStrings(string filePath, char separator = ' ')
+    {
+        return ReadFile(filePath).ToList().Select(x => x.Split(separator).AsEnumerable()).AsEnumerable();
+    }
+
+    /// <summary>
+    /// Reads a list of numbers from a file and splits each line at a separator, returning a 2D list
     /// </summary>
     /// <param name="filePath">Full path to input file</param>
     /// <param name="separator">Separator to split each line at. Uses a comma by default</param>
     /// <returns>A 2D list of integers where the first dimension is a line in the file and the second dimension is the separated row</returns>
-    public static List<List<int>> ReadListNums(string filePath, char separator = ',')
+    public static IEnumerable<IEnumerable<int>> ReadListNums(string filePath, char separator = ',')
     {
-        
+        return ReadFile(filePath).ToList().Select(x => x.Split(separator).Select(int.Parse).AsEnumerable()).AsEnumerable();
     }
 
     /// <summary>
@@ -39,9 +51,9 @@ public static class CommonLib
     /// <param name="filePath">Full path to input file</param>
     /// <param name="separator">Separator to split each line at. Uses a comma by default</param>
     /// <returns>A 2D list of characters where the first dimension is a line in the file and the second dimension is the separated row</returns>
-    public static List<List<char>> ReadListChars(string filePath, char separator = ',')
+    public static IEnumerable<IEnumerable<char>> ReadListChars(string filePath, char separator = ',')
     {
-
+        return ReadFile(filePath).ToList().Select(x => x.Split(separator).Select(char.Parse).AsEnumerable()).AsEnumerable();
     }
 
     /// <summary>
@@ -49,33 +61,22 @@ public static class CommonLib
     /// </summary>
     /// <param name="filePath">Full path to input file</param>
     /// <returns>A 2D list of digits where the first dimension is a line in the file and the second dimension is the number, split into individual digits</returns>
-    public static List<List<int>> ReadListDigits(string filePath)
+    public static IEnumerable<IEnumerable<int>> ReadListDigits(string filePath)
     {
-
+        return ReadFile(filePath).Select(line => line.Select(c => int.Parse(c.ToString())));
     }
-    
-    /// <summary>
-    /// Reads a list of strings from a file and splits each line at a separator, returning a 2D array
-    /// </summary>
-    /// <param name="filePath">Full path to input file</param>
-    /// <param name="separator">Separator to split each line at. Uses a space by default</param>
-    /// <returns>A 2D list of strings where the first dimension is a line in the file and the second dimension is each word on the line</returns>
-    public static List<List<string>> ReadListStrings(string filePath, char separator = ' ')
-    {
 
-    }
-    
     /// <summary>
     /// Prints a 2D array (matrix) of strings
     /// </summary>
     /// <param name="matrix">2D array (matrix) to print</param>
     public static void PrintMatrix<T>(List<List<T>> matrix)
     {
-        for (int i = 0; i < matrix.Count; i++)
+        foreach (List<T> t in matrix)
         {
             for (int j = 0; j < matrix.Count; j++)
             {
-                Console.Write(matrix[i][j] + "\t");
+                Console.Write(t[j] + "\t");
             }
             Console.WriteLine();
         }
