f4e61d8a0a38969015f264104380b1bbd182a216
Nicholas Rosati <nicholas@rosati.xyz>
2022-11-30 12:47:19 -0500

    tweaks mainly

--- CommonLib/CommonLib.cs
+++ CommonLib/CommonLib.cs
@@ -3,7 +3,7 @@
 public static class CommonLib
 {
     /// <summary>
-    /// Reads a file's contents
+    /// Reads a file's contents line by line into entries in the List
     /// </summary>
     /// <param name="filePath">Full path to input file</param>
     /// <returns>List of strings where each line in the file is an entry in the list</returns>
@@ -27,8 +27,8 @@ public static class CommonLib
     /// </summary>
     /// <param name="filePath">Full path to input file</param>
     /// <param name="separator">Separator to split each line at. Uses a comma by default</param>
-    /// <returns>A 2D array of integers where each line in the file is a number in the list</returns>
-    public static int[][] ReadListNums(string filePath, char separator = ',')
+    /// <returns>A 2D list of integers where the first dimension is a line in the file and the second dimension is the separated row</returns>
+    public static List<List<int>> ReadListNums(string filePath, char separator = ',')
     {
         
     }
@@ -38,39 +38,47 @@ public static class CommonLib
     /// </summary>
     /// <param name="filePath">Full path to input file</param>
     /// <param name="separator">Separator to split each line at. Uses a comma by default</param>
-    /// <returns>A 2D array of characters where each line in the file is a number in the list</returns>
-    public static char[][] ReadListChars(string filePath, char separator = ',')
+    /// <returns>A 2D list of characters where the first dimension is a line in the file and the second dimension is the separated row</returns>
+    public static List<List<char>> ReadListChars(string filePath, char separator = ',')
     {
 
     }
 
     /// <summary>
-    /// Reads a list of strings from a file and splits each line at a separator, returning a 2D array
+    /// Reads a list of numbers from a file and splits each number into digits, returning a 2D array
     /// </summary>
     /// <param name="filePath">Full path to input file</param>
-    /// <param name="separator">Separator to split each line at. Uses a space by default</param>
-    /// <returns>A 2D array of strings where each line in the file is a number in the list</returns>
-    public static char[][] ReadListStrings(string filePath, char separator = ' ')
+    /// <returns>A 2D list of digits where the first dimension is a line in the file and the second dimension is the number, split into individual digits</returns>
+    public static List<List<int>> ReadListDigits(string filePath)
     {
 
     }
     
     /// <summary>
-    /// Prints a 2D array of strings
+    /// Reads a list of strings from a file and splits each line at a separator, returning a 2D array
     /// </summary>
-    /// <param name="m">2D array of strings</param>
-    public static void PrintMatrix(string[][] m)
+    /// <param name="filePath">Full path to input file</param>
+    /// <param name="separator">Separator to split each line at. Uses a space by default</param>
+    /// <returns>A 2D list of strings where the first dimension is a line in the file and the second dimension is each word on the line</returns>
+    public static List<List<string>> ReadListStrings(string filePath, char separator = ' ')
     {
-        
+
     }
-
+    
     /// <summary>
-    /// Prints a 2D array of integers
+    /// Prints a 2D array (matrix) of strings
     /// </summary>
-    /// <param name="m">2D array of integers</param>
-    public static void PrintMatrix(int[][] m)
+    /// <param name="matrix">2D array (matrix) to print</param>
+    public static void PrintMatrix<T>(List<List<T>> matrix)
     {
-
+        for (int i = 0; i < matrix.Count; i++)
+        {
+            for (int j = 0; j < matrix.Count; j++)
+            {
+                Console.Write(matrix[i][j] + "\t");
+            }
+            Console.WriteLine();
+        }
     }
 
     /// <summary>
@@ -79,17 +87,9 @@ public static class CommonLib
     /// <param name="input">Input array</param>
     /// <param name="target">Target to remove from the array</param>
     /// <typeparam name="T">Type of the input array and removal target</typeparam>
-    public static void RemoveFromArray<T>(T?[] input, T target) where T : IComparable
+    public static void RemoveFromArray<T>(List<T> input, T target) where T : IComparable
     {
-        for (int i = 0; i < input.Length; i++)
-        {
-            if (input[i] == null) continue;
-            
-            if (input[i]!.CompareTo(target) == 0)
-            {
-                input[i] = default;
-            }
-        }
+        input.RemoveAll(e => e.CompareTo(target) == 0);
     }
 
     /// <summary>
@@ -98,7 +98,7 @@ public static class CommonLib
     /// <param name="input">Input array</param>
     /// <param name="targets">Target to remove from the array</param>
     /// <typeparam name="T">Type of the input array and removal target</typeparam>
-    public static void RemoveFromArray<T>(T?[] input, T[] targets) where T : IComparable
+    public static void RemoveFromArray<T>(List<T> input, IEnumerable<T> targets) where T : IComparable
     {
         foreach (T target in targets)
         {
@@ -112,9 +112,9 @@ public static class CommonLib
     /// <param name="input">Input array</param>
     /// <param name="target">Target to remove from the array</param>
     /// <typeparam name="T">Type of the input array and removal target</typeparam>
-    public static void RemoveFromArray<T>(T?[][] input, T target) where T : IComparable
+    public static void RemoveFromArray<T>(List<List<T>> input, T target) where T : IComparable
     {
-        foreach (T?[] row in input)
+        foreach (List<T> row in input)
         {
             RemoveFromArray(row, target);
         }
@@ -126,7 +126,7 @@ public static class CommonLib
     /// <param name="input">Input array</param>
     /// <param name="targets">Target to remove from the array</param>
     /// <typeparam name="T">Type of the input array and removal target</typeparam>
-    public static void RemoveFromArray<T>(T?[][] input, T[] targets) where T : IComparable
+    public static void RemoveFromArray<T>(List<List<T>> input, IEnumerable<T> targets) where T : IComparable
     {
         foreach (T target in targets)
         {
