cc551e47b5bc955c6a946de3c8732b7c8451ecf2
Nicholas Rosati <nicholas@rosati.xyz>
2022-12-01 00:46:44 -0500

    day 1 complete

--- D1/Program.cs
+++ D1/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using static CommonLib.CommonLib;
 
-Console.WriteLine("Hello, World!");
+var data = ReadFile(args[0]);
+Dictionary<int, int> carrying = new() { { 0, 0 } };
+var elfNumber = 0;
+
+foreach (string row in data)
+{
+    if (row == "")
+    {
+        carrying.Add(++elfNumber, 0);
+    }
+    else
+    {
+        carrying[elfNumber] += int.Parse(row);
+    }
+}
+
+var first = carrying.AsParallel().MaxBy(e => e.Value);
+carrying[first.Key] = -1;
+var second = carrying.AsParallel().MaxBy(e => e.Value);
+carrying[second.Key] = -1;
+var third = carrying.AsParallel().MaxBy(e => e.Value);
+
+Console.WriteLine($"{first}\n{second}\n{third}\nTotal: {first.Value + second.Value + third.Value}");
