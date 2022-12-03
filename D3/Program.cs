193198e53a6ad6856d1e6b84698597b9ce5c9293
Nicholas Rosati <nicholas@rosati.xyz>
2022-12-03 15:16:16 -0500

    day 3 done

--- D3/Program.cs
+++ D3/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using static CommonLib.CommonLib;
 
-Console.WriteLine("Hello, World!");
+var input = ReadFileColumn<string>(args[0]).ToList();
+
+var prioritiesSum = input
+                 .Select(x => x.Chunk(x.Length / 2))
+                 .Select(row =>
+                 {
+                     var rowList = row.ToList();
+                     return rowList.First().Intersect(rowList.Last()).First();
+                 })
+                 .Select(c => char.IsLower(c) ? c - 96 : c - 38)
+                 .Sum();
+Console.WriteLine($"(Part 1) Priority sum: {prioritiesSum}");
+
+var groupBadgesSum = input
+                  .Chunk(3)
+                  .Select(g => g[0].Intersect(g[1]).Intersect(g[2]).First())
+                  .Select(c => char.IsLower(c) ? c - 96 : c - 38)
+                  .Sum();
+Console.WriteLine($"(Part 2) Priority sum: {groupBadgesSum}");
