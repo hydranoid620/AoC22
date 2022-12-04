d433d630b60c7ba07ef37f255e8af0bedd7664cf
Nicholas Rosati <nicholas@rosati.xyz>
2022-12-04 01:37:19 -0500

    day 4 done

--- D4/Program.cs
+++ D4/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using static CommonLib.CommonLib;
 
-Console.WriteLine("Hello, World!");
+var data = ReadFileMatrix<string>(args[0]);
+var subsets = 0;
+var overlaps = 0;
+
+foreach (var row in data)
+{
+    var splitRow = row.Select(x =>
+                      {
+                          var split = x.Split('-').Select(int.Parse).ToArray();
+                          return Enumerable.Range(split[0], split[1] - split[0] + 1);
+                      })
+                      .ToArray();
+    
+    // if A - B != { } then A is not a subset of B
+    // if B - A != { } then B is not a subset of A
+    if (!splitRow[0].Except(splitRow[1]).Any() || !splitRow[1].Except(splitRow[0]).Any()) subsets++;
+    
+    if (splitRow[0].Intersect(splitRow[1]).Any()) overlaps++;
+}
+
+Console.WriteLine($"(Part 1) Subsets: {subsets}");
+Console.WriteLine($"(Part 2) Overlaps: {overlaps}");
