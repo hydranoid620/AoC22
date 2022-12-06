e1377a0909090956d451857137dac71cf26416d4
Nicholas Rosati <nicholas@rosati.xyz>
2022-12-06 00:53:03 -0500

    day 6 done

--- D6/Program.cs
+++ D6/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static CommonLib.CommonLib;
+
+var input = ReadFile(args[0]).First();
+
+// Part 1
+for (var i = 0; i < input.Length - 4; i++)
+{
+    if (input.Substring(i, 4).Distinct().Count() == 4)
+    {
+        Console.WriteLine(i + 4);
+        break;
+    }
+}
+
+// Part 2
+for (var i = 0; i < input.Length - 14; i++)
+{
+    if (input.Substring(i, 14).Distinct().Count() == 14)
+    {
+        Console.WriteLine(i + 14);
+        break;
+    }
+}
