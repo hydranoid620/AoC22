998695aa11cbbd28aa573705c3d75e2ecaa9ab09
Nicholas Rosati <nicholas@rosati.xyz>
2022-12-07 15:57:00 -0500

    day 7 complete

--- D7/Program.cs
+++ D7/Program.cs
@@ -1,66 +1,64 @@
 ﻿using static CommonLib.CommonLib;
 
 const int MAX_FOLDER_SIZE = 100000;
+const int TOTAL_DISK_SPACE = 70000000;
+const int REQUIRED_DISK_SPACE = 30000000;
 
-IEnumerable<string> input = ReadFileColumn<string>(args[0]).Skip(1); //TODO: switch to read file matrix, then dont need to do line splitting
+var input = ReadFileMatrix<string>(args[0], ' ').Skip(1).Select(x => x.ToArray());
 
 FolderNode rootNode = new(null, "/");
 FolderNode currentNode = rootNode;
 
 // Build directory tree
-foreach (string line in input)
+foreach (var line in input)
 {
-    string[] splitLine = line.Split(' ');
-
-    if (splitLine[0] == "$") // Is a command
+    if (line[0] == "$" && line[1] == "cd") // Is a command
     {
-        if (splitLine[1] == "cd")
-        {
-            currentNode =  splitLine[2] == ".." ? currentNode.Parent! : (FolderNode) currentNode.Children.Find(node => node.Name == splitLine[2])!;
-        }
+        currentNode = line[2] == ".." ? currentNode.Parent! : (FolderNode) currentNode.Children.Find(node => node.Name == line[2])!;
     }
     else // Is output (from ls)
     {
-        currentNode.Children.Add(int.TryParse(splitLine[0], out int size) ? 
-                                     new FileNode(currentNode, size, splitLine[1]) :
-                                     new FolderNode(currentNode, splitLine[1]));
+        currentNode.Children.Add(int.TryParse(line[0], out int size) ? 
+                                     new FileNode(currentNode, size, line[1]) :
+                                     new FolderNode(currentNode, line[1]));
     }
 }
 
 currentNode = rootNode;
 
+// Part 1
 var sizeTotal = 0;
-var nodes = currentNode.Children.Where(node => node is FolderNode).Cast<FolderNode>().ToList();
-var nodesToProcess = new List<FolderNode>();
-while (nodes.Count > 0)
+var currentNodes = currentNode.Children.Where(node => node is FolderNode).Cast<FolderNode>().ToList();
+var nextNodes = new List<FolderNode>();
+while (currentNodes.Count > 0)
 {
-    foreach (var node in nodes)
+    foreach (FolderNode node in currentNodes)
     {
         if (node.Size <= MAX_FOLDER_SIZE) sizeTotal += node.Size;
-        nodesToProcess.AddRange(node.Children.Where(n => n is FolderNode).Cast<FolderNode>().ToList());
+        nextNodes.AddRange(node.Children.Where(n => n is FolderNode).Cast<FolderNode>().ToList());
     }
 
-    nodes.Clear();
-    nodes = nodesToProcess.Select(x => x).ToList();
-    nodesToProcess.Clear();
+    currentNodes = nextNodes.Select(x => x).ToList();
+    nextNodes.Clear();
 }
-
 Console.WriteLine(sizeTotal);
 
+// Part 2
+FolderNode directoryToDelete = rootNode;
+currentNodes = currentNode.Children.Where(node => node is FolderNode).Cast<FolderNode>().ToList();
+nextNodes.Clear();
+while (currentNodes.Count > 0)
+{
+    foreach (FolderNode node in currentNodes)
+    {
+        if (TOTAL_DISK_SPACE - rootNode.Size + node.Size >= REQUIRED_DISK_SPACE && node.Size < directoryToDelete.Size) directoryToDelete = node;
+        nextNodes.AddRange(node.Children.Where(n => n is FolderNode).Cast<FolderNode>().ToList());
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    currentNodes = nextNodes.Select(x => x).ToList();
+    nextNodes.Clear();
+}
+Console.WriteLine(directoryToDelete.Size);
 
 internal class Node
 {
@@ -75,7 +73,7 @@ internal class Node
         Name = name;
     }
 
-    public void AddSize(int size)
+    private void AddSize(int size)
     {
         Size += size;
         Parent?.AddSize(size);
